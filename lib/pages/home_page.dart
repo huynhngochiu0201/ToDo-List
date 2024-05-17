b014e924468494ebb959df4be044cdb0e79f8b94
Huỳnh Hiếu <huynhhieu@MacBook-Pro-cua-Huynh.local>
2024-05-17 14:10:35 +0700

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,8 +18,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController searchController = TextEditingController();
   TextEditingController editingController = TextEditingController();
+  TextEditingController addController = TextEditingController();
+  FocusNode addFocus = FocusNode();
   List<TodoModel> todos = [];
   List<TodoModel> searchList = [];
+  bool showAddBox = false;
 
   @override
   void initState() {
@@ -43,41 +46,140 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         backgroundColor: AppColor.bgColor,
         appBar: _buildAppBar(context),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: TdSearchBox(
-                  controller: searchController,
-                  onChanged: _search,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TdSearchBox(
+                        controller: searchController,
+                        onChanged: _search,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    _divider(),
+                    Expanded(
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0)
+                            .copyWith(top: 16.0, bottom: 98.0),
+                        itemBuilder: (context, index) {
+                          final todo = searchList.reversed.toList()[index];
+                          return TodoItem(
+                            todo,
+                            onTap: () {
+                              todo.isDone = !(todo.isDone ?? false);
+                              setState(() {});
+                            },
+                            onEditing: () => _editing(context, todo),
+                            onDelete: () => _delele(context, todo),
+                          );
+                        },
+                        separatorBuilder: (_, __) =>
+                            const SizedBox(height: 20.0),
+                        itemCount: searchList.length,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 16.0),
-              _divider(),
-              Expanded(
-                child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0)
-                      .copyWith(top: 16.0, bottom: 98.0),
-                  itemBuilder: (context, index) {
-                    final todo = searchList.reversed.toList()[index];
-                    return TodoItem(
-                      todo,
-                      onTap: () {
-                        todo.isDone = !(todo.isDone ?? false);
-                        setState(() {});
-                      },
-                      onEditing: () => _editing(context, todo),
-                      onDelete: () => _delele(context, todo),
-                    );
-                  },
-                  separatorBuilder: (_, __) => const SizedBox(height: 20.0),
-                  itemCount: searchList.length,
-                ),
+            ),
+            Positioned(
+              left: 20.0,
+              right: 20.0,
+              bottom: 20.0,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Visibility(
+                      visible: showAddBox,
+                      child: _addBox(),
+                    ),
+                  ),
+                  const SizedBox(width: 12.0),
+                  _addButton(),
+                ],
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _addButton() {
+    return GestureDetector(
+      onTap: () {
+        if (showAddBox == false) {
+          showAddBox = true;
+          setState(() {});
+          addFocus.requestFocus();
+        } else {
+          String text = addController.text.trim();
+          if (text.isEmpty) {
+            showAddBox = false;
+            setState(() {});
+            addFocus.unfocus();
+          } else {
+            TodoModel todo = TodoModel()
+              ..id = '${DateTime.now().millisecondsSinceEpoch}'
+              ..text = text
+              ..isDone = false;
+
+            todos.add(todo);
+            _search('');
+            addController.clear();
+            searchController.clear();
+            addFocus.unfocus();
+            showAddBox = false;
+            setState(() {});
+          }
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: AppColor.orange,
+          border: Border.all(color: AppColor.red, width: 1.2),
+          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+          boxShadow: const [
+            BoxShadow(
+              color: AppColor.shadow,
+              offset: Offset(0.0, 3.0),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: const Icon(Icons.add, size: 32.6, color: AppColor.white),
+      ),
+    );
+  }
+
+  Widget _addBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      decoration: BoxDecoration(
+        color: AppColor.white,
+        border: Border.all(color: AppColor.orange, width: 1.2),
+        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+        boxShadow: const [
+          BoxShadow(
+            color: AppColor.shadow,
+            offset: Offset(0.0, 3.0),
+            blurRadius: 6.0,
           ),
+        ],
+      ),
+      child: TextField(
+        controller: addController,
+        focusNode: addFocus,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Add a new todo item',
+          hintStyle: TextStyle(color: AppColor.grey),
         ),
       ),
     );
